6d5b843106650d98f0e42911e57020de03f34f7a
Abdelrahman Mohamed <abdo.sf.ahmed@gmail.com>
2026-04-21 04:06:11 +0200

    fix github pages routing

--- abdo-portfolio/next.config.ts
+++ abdo-portfolio/next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
+
 const nextConfig: NextConfig = {
-  output: "export",
-  trailingSlash: true,
+  output: 'export',
+  basePath: '/abdo-portfolio',
+  assetPrefix: '/abdo-portfolio/',
+  images: {
+    unoptimized: true
+  }
 };
+
 export default nextConfig;
